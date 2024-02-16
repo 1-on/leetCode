9e0323e094f9b4bc70ac197d988ee01c30c4b2f2
一弦 <2721975880@qq.com>
2024-02-16 14:56:56 +0800

    p344 翻转字符串

---
+++ src/main/java/leetcode/editor/cn/P344_ReverseString.java
@@ -0,0 +1,64 @@
+//编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 s 的形式给出。 
+//
+// 不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = ["h","e","l","l","o"]
+//输出：["o","l","l","e","h"]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = ["H","a","n","n","a","h"]
+//输出：["h","a","n","n","a","H"] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁵ 
+// s[i] 都是 ASCII 码表中的可打印字符 
+// 
+//
+// Related Topics 双指针 字符串 👍 885 👎 0
+
+
+package leetcode.editor.cn;
+
+/**
+ * 反转字符串
+ *
+ * @author DY
+ * @date 2024-02-16 13:38:47
+ */
+public class P344_ReverseString {
+    public static void main(String[] args) {
+        //测试代码
+        Solution solution = new P344_ReverseString().new Solution();
+    }
+
+    //力扣代码
+//leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void reverseString(char[] s) {
+            if (s.length == 1) {
+                System.out.println(s.toString());
+            }
+            int n = s.length;
+            for (int i = 0; i < n / 2; i++) {
+                char x = s[i];
+                s[i] = s[n - 1 - i];
+                s[n - 1 - i] = x;
+            }
+            System.out.println(s.toString());
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
